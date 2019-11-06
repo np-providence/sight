4db4c8fe014af2de146789621c4a735cbdeb93ee
Sean Lim <seanlimdev856@gmail.com>
2019-11-06 10:04:19 +0800

    Add basic picamera stuff

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from uuid import getnode as get_mac
+from picamera import PiCamera 
+from time import sleep
+from io import BytesIO
+
 if __name__ == "__main__":
-    print("Hello world!")
+    # Create an in-memory stream
+    my_stream = BytesIO()
+    camera = PiCamera()
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(2)
+    camera.capture(my_stream, 'jpeg')
