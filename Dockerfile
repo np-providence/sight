ce71834c316d34656619d8774ec8831d0a1c4b4c
Sean Lim <seanlimdev856@gmail.com>
2019-11-26 10:05:50 +0800

    Switch to poetry

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python:3
-RUN pip install pipenv 
-COPY Pipfile* ./
-RUN pipenv install
-COPY main.py .
-CMD pipenv run python3 main.py
+RUN pip install poetry
+COPY ./ ./
+RUN poetry install
+CMD poetry run python3 main.py
--- capture/picamera.py
+++ capture/picamera.py
@@ -1,11 +1,13 @@
-import picamera
+#import picamera
 
 # Pi Runtime 
+#def capture():
+#    with picamera.PiCamera() as camera:
+#        camera.framerate = 24
+#        time.sleep(2) # Warm up
+#        while True:
+#            image = []
+#            camera.capture(image, 'bgr')
+#            yield image
 def capture():
-    with picamera.PiCamera() as camera:
-        camera.framerate = 24
-        time.sleep(2) # Warm up
-        while True:
-            image = []
-            camera.capture(image, 'bgr')
-            yield image
+    pass
